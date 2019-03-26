cc7976c0b66b880a3dd568dd0064f0b673fd3d81
hobetuz <hobetuz@ipartekaula.com>
2019-03-26 19:21:04 +0100

    .

--- Equipos/src/app/Pipes/team-filter.pipe.ts
+++ Equipos/src/app/Pipes/team-filter.pipe.ts
@@ -2,13 +2,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Equipo } from './../Models/Equipo';
 
 @Pipe({
-  name: 'teamFilter',
-  pure: false
+  name: 'teamFilter'
 })
 export class TeamFilterPipe implements PipeTransform {
   transform(itemEquipos: Array<Equipo>, filtroNombre?: string): any {
-    console.log(itemEquipos);
-    console.log(filtroNombre);
     return itemEquipos.filter(e => e.Nombre.toLocaleLowerCase().indexOf(filtroNombre.toLocaleLowerCase()) !== -1);
   }
 }
